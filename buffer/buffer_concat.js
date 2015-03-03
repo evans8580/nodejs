87d2d6b09cfcc7cf5ae682c870cb6a2801b2c5b7
Junho Lee <evans8580@gmail.com>
2015-03-03 13:42:02 +0900

    buffer, stream and zlib

---
+++ buffer/buffer_concat.js
@@ -0,0 +1,8 @@
+/**
+ * New node file
+ */
+var af = new Buffer("African Swallow?");
+var eu = new Buffer("European Swallow?");
+var question = new Buffer("Air Speed Velocity of an ");
+console.log(Buffer.concat([question, af]).toString());
+console.log(Buffer.concat([question, eu]).toString());
