447d051873603728b60c1845669a15bb917ebfa0
Junho Lee <evans8580@gmail.com>
2015-03-04 18:27:25 +0900

    file & http

---
+++ file/file_write_sync.js
@@ -0,0 +1,12 @@
+/**
+ * New node file
+ */
+var fs = require('fs');
+var veggieTray = ['carrots', 'celery', 'olives'];
+fd = fs.openSync('veggie.txt', 'w');
+while (veggieTray.length) {
+	veggie = veggieTray.pop() + " ";
+	var bytes = fs.writeSync(fd, veggie, null, null);
+	console.log("Wrote %s %dbytes", veggie, bytes);
+}
+fs.closeSync(fd);
---
+++ http/http_client_static.js
@@ -0,0 +1,21 @@
+/**
+ * New node file
+ */
+var http = require('http');
+var options = {
+		hostname: 'localhost',
+		port: '8088',
+		path: '/hello.html'
+};
+function handleResponse(response) {
+	var serverData = '';
+	response.on('data', function (chunk) {
+		serverData += chunk;
+	});
+	response.on('end', function () {
+		console.log(serverData);
+	});
+}
+http.request(options, function(response) {
+	handleResponse(response);
+}).end();
